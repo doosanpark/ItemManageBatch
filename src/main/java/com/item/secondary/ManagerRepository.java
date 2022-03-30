27c3b8430782554c9e3ea486f92f3cb8caba7094
doosanpark <erid3232@naver.com>
2022-03-31 00:03:43 +0900

    add query in manager repository

--- src/main/java/com/item/secondary/ManagerRepository.java
+++ src/main/java/com/item/secondary/ManagerRepository.java
@@ -1,7 +1,16 @@
 package com.item.secondary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
 
+	
+	@Modifying
+    @Query(value = "insert into item_acqird (itm_seq, itm_nm, itm_cnt) VALUES (:itm_seq,:itm_nm,:itm_cnt)", nativeQuery = true)
+    @Transactional
+    void saveItms(@Param("itm_seq") int itm_seq, @Param("itm_nm") String itm_nm, @Param("itm_cnt") int itm_cnt);
 }
